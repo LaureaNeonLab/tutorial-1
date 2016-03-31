7e5ff25a7d4d39073edf114e4a9502946c88cb20
adokas <alicelik93@yahoo.com>
2016-03-31 13:48:04 +0300

    adokas-tutorial1

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,11 @@
 // Tutorial-1 of Laurea Implementing mobile apps.
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires' thats the part in these brackets [ ].
-// This means when our angular app starts it requires the ionic lib to function correctly.
 var myApp = angular.module('start', ['ionic'])
 
 myApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // Don't remove this line unless you know what you are doing. It stops the viewport
@@ -24,27 +19,46 @@ myApp.run(function($ionicPlatform) {
   });
 })
 // ABOVE THIS LINE CHANGE NOTHING
-/*
-    In the repository readme located here https://github.com/LaureaNeonLab/tutorial-1 
-    Follow all the instructions inserting the code snippets as instructed.
-    Feel free to experiment and go off topic as this is the best way to test and learn.
-    The code can always be re-cloned.
-    */
+
 myApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-  
-    .state('pOne', {
-      url: "/p1",
-      templateUrl: "pages/pOne.html",
-    })
 
-    .state('home', {
+    .state('pOne', {
+  url: "/p1",
+  templateUrl: "pages/pone.html",
+})
+
+    .state('pTwo', {
+  url: "/p2",
+  templateUrl: "pages/ptwo.html",
+})
+
+    .state('pThree', {
+  url: "/p3",
+  templateUrl: "pages/pthree.html",
+})
+
+    .state('pFour', {
+  url: "/p4",
+  templateUrl: "pages/pfour.html",
+})
+
+    .state('pFive', {
+  url: "/p5",
+  templateUrl: "pages/pfive.html",
+})
+
+   state('home', {
       url: "/home",
       abstract: true,
-      templateUrl: "index.html",
+      templateUrl: "home.html",
     });
-    
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/index.html');
+
+  /* 
+    The line below is a catch all that returns you to the '/home' state if we dont find any others.
+    We only have one state so thats not relevant yet.
+  */ 
+  $urlRouterProvider.otherwise('/home');
 });
+
 
